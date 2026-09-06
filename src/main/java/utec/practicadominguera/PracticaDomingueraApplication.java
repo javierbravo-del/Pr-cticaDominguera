bf609eb94662267fd0007c18db9eee1575840f79
Javier Bravo <javier.bravo@utec.edu.pe>
2026-09-05 22:38:37 -0500

    first commit

---
+++ src/main/java/utec/practicadominguera/PracticaDomingueraApplication.java
@@ -0,0 +1,13 @@
+package utec.practicadominguera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaDomingueraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaDomingueraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utec/practicadominguera/PracticaDomingueraApplicationTests.java
@@ -0,0 +1,13 @@
+package utec.practicadominguera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaDomingueraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
